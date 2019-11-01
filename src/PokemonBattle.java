5260fa7465d416e3995232c6932a2ba2aa86c7b6
questdyl000 <questdyl000@CAM-LAB-22.bgsd.k12.wa.us>
2019-11-01 07:48:50 -0700

    Work in progress

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -1,8 +1,28 @@
+/*		Pseudo-code:
+ * -----------------------------------
+ * method battlestart():
+ * 	  Announce the battle starting
+ * 	  Ask for Pokemon name from user
+ * 	  Announce the battle starting
+ * -----------------------------------
+ * method damage()
+ * 	  damage = ((2 * lvl + 10) / 250 + (atk / def) * (base + 2)) * modifier
+ * 	  modifier = STAB * rng (0.85, 1.0)
+ * 	  Ask for user input in stats:
+ * 		Level:
+ *		Attack:
+ *		Defense:
+ *		Base:
+ * 		STAB:
+ *		HP:
+ * 	  return HP
+ */
+
 
 public class PokemonBattle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
