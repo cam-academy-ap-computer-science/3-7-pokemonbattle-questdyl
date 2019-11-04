60581a2ca79db3df111abcf57d0518a837faca55
questdyl000 <questdyl000@CAM-LAB-22.bgsd.k12.wa.us>
2019-11-04 07:52:31 -0800

    WIP

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -3,8 +3,7 @@
  * method battlestart():
  * 	  Announce the battle starting
  * 	  Ask for Pokemon name from user
- * 	  Announce the battle starting
- * -----------------------------------
+ * ----------------------------------
  * method damage()
  * 	  damage = ((2 * lvl + 10) / 250 + (atk / def) * (base + 2)) * modifier
  * 	  modifier = STAB * rng (0.85, 1.0)
@@ -16,6 +15,9 @@
  * 		STAB:
  *		HP:
  * 	  return HP
+ * ----------------------------------
+ * method statsTable()
+ * 	  
  */
 
 
